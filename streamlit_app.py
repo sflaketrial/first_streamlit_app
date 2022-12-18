830244bf5622567d7c06532d26a31db4ab3a870b
sflaketrial <120910220+sflaketrial@users.noreply.github.com>
2022-12-18 04:07:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title("My Parents New Healthy Dinner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 and Blueberry Oatmeal")
+streamlit.text("Kale, Spinach and Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
